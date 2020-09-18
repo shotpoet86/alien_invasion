6774503e90aaceabed7b4b5cb3cd2c60fd88f4d0
shotpoet86 <curlybracemaster@gmail.com>
2020-09-18 16:14:21 -0400

    added movement, bullets

--- alien_invasion.py
+++ alien_invasion.py
@@ -2,6 +2,7 @@ import pygame
 import sys
 from settings import Settings
 from ship import Ship
+from bullet import Bullet
 
 
 class AlienInvasion:
@@ -12,17 +13,22 @@ class AlienInvasion:
         pygame.init()
         # using settings from seperate settings class
         self.settings = Settings()
-        self.screen = pygame.display.set_mode(
-            (self.settings.screen_width, self.settings.screen_height))
-        self.screen = pygame.display.set_mode((1200, 800))
+        self.screen = pygame.display.set_mode((self.settings.screen_width, self.settings.screen_height))
+        # self.screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
+        # self.settings.screen_width = self.screen.get_rect().width
+        # self.settings.screen_height = self.screen.get_rect().height
         pygame.display.set_caption('Alien Invasion')
         self.ship = Ship(self)
+        self.bullets = pygame.sprite.Group()
 
     def run_game(self):
         """starts main loop of game"""
         while True:
             self._check_events()
             self._update_screen()
+            # calls update method from Ship class
+            self.ship.update()
+            self.bullets.update()
 
     def _check_events(self):
         """update images on screen, and flips to new screen"""
@@ -30,14 +36,40 @@ class AlienInvasion:
             if event.type == pygame.QUIT:
                 sys.exit()
             elif event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_RIGHT:
-                    self.ship.rect.x += 1
-                    # moves ship to right
+                self._check_keydown_events(event)
+            elif event.type == pygame.KEYUP:
+                self._check_keyup_events(event)
+
+    def _check_keydown_events(self, event):
+        """respond to keypress"""
+        if event.key == pygame.K_RIGHT:
+            self.ship.moving_right = True
+        elif event.key == pygame.K_LEFT:
+            self.ship.moving_left = True
+        # closes game with "q" keypress
+        elif event.key == pygame.K_q:
+            sys.exit()
+        elif event.key == pygame.K_SPACE:
+            self._fire_bullet()
+
+    def _check_keyup_events(self, event):
+        """respond to key release"""
+        if event.key == pygame.K_RIGHT:
+            self.ship.moving_right = False
+        elif event.type == pygame.KEYUP:
+            self.ship.moving_left = False
+
+    def _fire_bullet(self):
+        # creates new bullet and adds it to the bullet group
+        new_bullet = Bullet(self)
+        self.bullets.add(new_bullet)
 
     def _update_screen(self):
         # redraws screen during each pass of loop
         self.screen.fill(self.settings.background_color)
         self.ship.blitme()
+        for bullet in self.bullets.sprites():
+            bullet.draw_bullet()
         # makes the most recently drawn screen visible
         pygame.display.flip()
 
--- ship.py
+++ ship.py
@@ -6,13 +6,30 @@ class Ship:
     def __init__(self, ai_game):
         # initializes ship and its start point
         self.screen = ai_game.screen
+        self.settings = ai_game.settings
         self.screen_rect = ai_game.screen.get_rect()
 
         # loads ship image and gets its rect
         self.image = pygame.image.load('ship.bmp')
         self.rect = self.image.get_rect()
+
         # starts each new ship at bottom center of window
         self.rect.midbottom = self.screen_rect.midbottom
+
+        # stores ships horizontal decimal value position
+        self.x = float(self.rect.x)
+
+        # movement flag
+        self.moving_right = False
+        self.moving_left = False
+
+    def update(self):
+        # update ship position based on movement flag
+        if self.moving_right and self.rect.right < self.screen_rect.right:
+            self.x += self.settings.ship_speed
+        if self.moving_left and self.rect.left > self.screen_rect.left:
+            self.x -= self.settings.ship_speed
+        self.rect.x = self.x
 
     def blitme(self):
         # draws ship at current location
